ebd65feec1d6234bd341b82d60a533494be0792c
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-18 18:25:09 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,19 @@
 def hello_t
-
+  if block_give?
+    i = 0
+    while i < array.length
+      yield array[i]
+      i = i + 1
+    end
+    array
+  else
+    puts "Hey! No block was given!"
+  end
 end
 
 # call your method here!
-
+hello_t(["Tim","Tom","jim"]) do
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
